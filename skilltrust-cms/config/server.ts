4a1fea97f1f90d85bf635b0176a0659cd312c15b
bartoszlysniewski <bartosz.lysniewski@gmail.com>
2025-03-18 10:06:21 +0100

    import { env } from 'some-env-library';
    const FLAG_NPS = Boolean(env('FLAG_NPS', true));
    const FLAG_PROMOTE_EE = Boolean(env('FLAG_PROMOTE_EE', true));
    const DATABASE_PORT = parseInt(env('DATABASE_PORT', '3306'), 10);
    const DATABASE_POOL_MIN = parseInt(env('DATABASE_POOL_MIN', '2'), 10);
    const DATABASE_POOL_MAX = parseInt(env('DATABASE_POOL_MAX', '10'), 10);
    const DATABASE_CONNECTION_TIMEOUT = parseInt(env('DATABASE_CONNECTION_TIMEOUT', '60000'), 10);
    const DATABASE_SSL = env('DATABASE_SSL', 'false') === 'true';
    const DATABASE_SSL_REJECT_UNAUTHORIZED = env('DATABASE_SSL_REJECT_UNAUTHORIZED', 'true') === 'true';
    const client = env('DATABASE_CLIENT', 'sqlite') as keyof typeof connections;
    const PORT = parseInt(env('PORT', '1337'), 10);

--- skilltrust-cms/config/server.ts
+++ skilltrust-cms/config/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }: { env: (key: string, defaultValue?: any) => any }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: parseInt(env('PORT', '1337'), 10),
   app: {
     keys: env.array('APP_KEYS'),
   },
