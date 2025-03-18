e3df5b9df3498dea60305ee0add19dc2a3ef3ca7
bartoszlysniewski <bartosz.lysniewski@gmail.com>
2025-03-18 12:28:06 +0100

    Sanity work

--- lib/sanity/client.ts
+++ lib/sanity/client.ts
@@ -1,8 +1,7 @@
-// lib/sanity/client.ts
 import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '0gon2nh4'
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03'
 
@@ -10,7 +9,7 @@ export const client = createClient({
     projectId,
     dataset,
     apiVersion,
-    useCdn: false, // Ustaw na `true` w produkcji dla lepszej wydajności
+    useCdn: true, // Ustaw na `true` w produkcji dla lepszej wydajności
 })
 
 const builder = imageUrlBuilder(client)
